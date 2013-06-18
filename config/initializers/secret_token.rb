553a0dd3092e154701032fd3677309c57e21933a
allthingsit <info@allthingsit.co.ke>
2013-06-18 18:22:10 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '2d60ffa245944b0adee098a1fc33f33bddd51d25cba184fd0ab964a1d87e8f5f9e9c510633df450e227473edcf854dbf4c780420ad1581805a7381e2c60dc5a7'
